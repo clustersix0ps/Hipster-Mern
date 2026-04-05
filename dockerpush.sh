05617d7136ee41f27fbffd402f8e5dcb41691444
clustersix0ps <saidevops753@gmail.com>
2026-04-05 16:35:51 +0530

    Update dockerpush.sh

--- dockerpush.sh
+++ dockerpush.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker build -t savitxr/adservice-hipster:latest ./src/adservice
 docker push savitxr/adservice-hipster:latest
+docker build -t savitxr/authservice-hipster:latest ./src/adservice
+docker push savitxr/authservice-hipster:latest
 docker build -t savitxr/cartservice-hipster:latest ./src/cartservice/src
 docker push savitxr/cartservice-hipster:latest
 docker build -t savitxr/checkoutservice-hipster:latest ./src/checkoutservice
@@ -11,8 +13,8 @@ docker build -t savitxr/emailservice-hipster:latest ./src/emailservice
 docker push savitxr/emailservice-hipster:latest
 docker build -t savitxr/frontend-hipster:latest ./src/frontend
 docker push savitxr/frontend-hipster:latest
-docker build -t savitxr/loadgenerator-hipster:latest ./src/loadgenerator
-docker push savitxr/loadgenerator-hipster:latest
+docker build -t savitxr/assistantservice-hipster:latest ./src/loadgenerator
+docker push savitxr/assistantservice-hipster:latest
 docker build -t savitxr/paymentservice-hipster:latest ./src/paymentservice
 docker push savitxr/paymentservice-hipster:latest
 docker build -t savitxr/productcatalogservice-hipster:latest ./src/productcatalogservice
